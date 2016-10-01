695c0a18e30c2c61fdb0448f3182aa2e8d24757a
Cory Wheeler <corywheeler@gmail.com>
2016-10-01 09:40:49 -0400

    A strategy implementation.

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -6,7 +6,49 @@ namespace Strategy
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello Strategy!");
+			CalculateClient client = new CalculateClient();
+
+			client.SetCalculate(new Minus());
+			Console.WriteLine("Minus: " + client.Calculate(8, 2));
+
+			client.SetCalculate(new Plus());
+			Console.WriteLine("Plus: " + client.Calculate(3, 2));
+		}
+	}
+
+	public class CalculateClient
+	{
+		private ICalculate strategy;
+
+		public int Calculate(int value1, int value2)
+		{
+			return strategy.Calculate(value1, value2);
+		}
+
+		public void SetCalculate(ICalculate strategy)
+		{
+			this.strategy = strategy;
+		}
+	}
+
+	public interface ICalculate
+	{
+		int Calculate(int value1, int value2);
+	}
+
+	public class Plus : ICalculate
+	{
+		public int Calculate(int value1, int value2)
+		{
+			return value1 + value2;
+		}
+	}
+
+	public class Minus : ICalculate
+	{
+		public int Calculate(int value1, int value2)
+		{
+			return value1 - value2;
 		}
 	}
 }
