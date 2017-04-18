288acb419baf69910f4d80f6f67aabc32f21b757
Cory Wheeler <corywheeler@gmail.com>
2017-04-18 19:56:11 -0400

    initial commit, with an implementation of ICircuitBreakerStateStore

---
+++ CircuitBreaker/CircuitBreaker/CircuitBreaker/ICircuitBreakerStateStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CircuitBreaker
+{
+	public interface ICircuitBreakerStateStore
+	{
+		CircuitBreakerStateEnum State { get; }
+
+		Exception LastException { get; }
+
+		DateTime LastStateChangedDateUtc { get; }
+
+		void Trip(Exception ex);
+
+		void Reset();
+
+		void HalfOpen();
+
+		bool IsClosed();
+	}
+}
---
+++ CircuitBreaker/CircuitBreaker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CircuitBreaker
+{
+	class MainClass
+	{
+		// Inspiration coming from:
+		// https://docs.microsoft.com/en-us/azure/architecture/patterns/circuit-breaker#example
+
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
