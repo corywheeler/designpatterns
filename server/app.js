00f46af664c7831736b9b3c2e471d7a8ea54a4ba
Cory Wheeler <corywheeler@gmail.com>
2017-04-21 06:29:31 -0400

    wiring up an initial rout for all circuit breaker related things

--- server/app.js
+++ server/app.js
@@ -1,3 +1,18 @@
 var express = require("express");
 
-console.log("hello");
+
+
+var app = express();
+var port = 6000;
+
+app.get('/', function(req, res) {
+  res.send('Server is up.')
+})
+
+app.get('/circuit-breaker', function(req, res) {
+  res.send('Circuit Breaker.')
+})
+
+app.listen(port, function(err) {
+  console.log('listening on port ', port);
+})
