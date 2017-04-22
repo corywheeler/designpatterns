258c51ba57bbbdc7ed5c9f5c710e7bd5f363892f
Cory Wheeler <corywheeler@gmail.com>
2017-04-21 21:29:31 -0400

    set up the circuit breaker endpoint to alternate between good and bad persiods of responses. put the server on a safer port for the browser

--- server/app.js
+++ server/app.js
@@ -1,16 +1,25 @@
 var express = require("express");
 
-
-
 var app = express();
-var port = 6000;
+var port = 5001;
+
+var requests = 0;
+var goodBadResponseStatusList = [200, 400, 400, 400, 400, 200, 200, 200, 400, 400, 400, 400, 400, 400, 400, 200, 200, 200, 200, 200];
 
 app.get('/', function(req, res) {
   res.send('Server is up.')
 })
 
 app.get('/circuit-breaker', function(req, res) {
-  res.send('Circuit Breaker.')
+  res.statusCode = goodBadResponseStatusList[requests % goodBadResponseStatusList.length];
+
+  var message = 'Circuit Breaker Request ' + requests;
+
+  res.send(message);
+
+  console.log(message);
+
+  requests++;
 })
 
 app.listen(port, function(err) {
