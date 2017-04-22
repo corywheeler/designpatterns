dcb59487c7a79db61a007abd23b301b967c4243e
Cory Wheeler <corywheeler@gmail.com>
2017-04-21 23:33:23 -0400

    modify some of the success/error counts.

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ var app = express();
 var port = 5001;
 
 var requests = 0;
-var goodBadResponseStatusList = [200, 400, 400, 400, 400, 200, 200, 200, 400, 400, 400, 400, 400, 400, 400, 200, 200, 200, 200, 200];
+var goodBadResponseStatusList = [200, 400, 400, 200, 200, 200, 200, 200, 400, 400, 400, 200, 200, 200, 400, 200, 200, 200, 200, 200];
 
 app.get('/', function(req, res) {
   res.send('Server is up.')
@@ -13,13 +13,13 @@ app.get('/', function(req, res) {
 app.get('/circuit-breaker', function(req, res) {
   res.statusCode = goodBadResponseStatusList[requests % goodBadResponseStatusList.length];
 
-  var message = 'Circuit Breaker Request ' + requests;
+  requests++;
+  var message = 'Circuit Breaker Request #' + requests;
 
   res.send(message);
 
   console.log(message);
 
-  requests++;
 })
 
 app.listen(port, function(err) {
